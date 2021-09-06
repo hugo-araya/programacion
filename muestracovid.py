a655da0011c76fc3cdf8887a809817064864d5e1
hugo-araya <hugo.araya.carrasco@gmail.com>
2021-09-06 10:21:10 -0300

    Create muestracovid.py

---
+++ muestracovid.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+
+def lectura(nombre):
+    ar = open(nombre)
+    fecha = ar.readline()
+    fecha = fecha.rstrip('\n')
+    X = fecha.split(',')
+#   X = ((ar.readline()).rstrip('\n')).split(',')
+    XX = list(range(len(X)))
+    casos = ar.readline()
+    casos = casos.rstrip('\n')
+    casos = casos.split(',')
+    Y = []
+    for elem in casos:
+        Y.append(float(elem))
+    return X, Y
+
+def graficar(X, Y):
+    plt.plot(X, Y)
+    plt.show()
+
+def finalizar(X, Y):
+    print('Son', len(Y), 'Casos' )
+
+if __name__ == "__main__":
+    X, Y = lectura('TotalesNacionalesResumen.csv')
+    graficar(X, Y)
+    finalizar(X, Y)
+#   xr, yr = resumen_por mes(X, Y)
